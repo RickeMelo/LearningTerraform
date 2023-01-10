d920929fc0aaf5916da9d4bd77f8da9c8bd3f669
Henrique Melo <jcavalca85@gmail.com>
2023-01-10 15:39:27 -0500

    update variaveis

--- main.tf
+++ main.tf
@@ -1,23 +1,14 @@
 terraform {
     required_providers {
       aws = {
-        sourcesource = "hashicorp/aws"
+        source = "hashicorp/aws"
         version = "~> 3.0"
       }
     }
 }
 
 provider "aws" {
-  region = "us-west-1"
-}
-
-resource "aws_instance" "demo" {
-  ami = "ami-00831fc7c1e3ddc60"
-  instance_type = "t2.micro"
-
-  tags = {
-    "name" = "Demo System"
-  }
+  region = "var.region"
 }
 
 variable "region" {
@@ -33,4 +24,17 @@ variable "ami" {
 variable "type" {
  default = "t2.micro"
  description = "Size of VM"
+}
+
+resource "aws_instance" "demo" {
+  ami = "ami-00831fc7c1e3ddc60"
+  instance_type = "t2.micro"
+
+  tags = {
+    "name" = "Demo System"
+  }
+}
+
+output "instance_id" {
+  instance = aws_instance.demo.id
 }
