f56b77d7b159b0925b6753eacd703aa27e8c7e30
Henrique Melo <jcavalca85@gmail.com>
2023-01-10 15:29:08 -0500

    variaveis aplicadas

--- main.tf
+++ main.tf
@@ -19,3 +19,18 @@ resource "aws_instance" "demo" {
     "name" = "Demo System"
   }
 }
+
+variable "region" {
+ default = "us-west-1"
+ description = "AWS Region"
+}
+
+variable "ami" {
+ default = "ami-00831fc7c1e3ddc60"
+ description = "Amazon Machine Image ID for Ubuntu Server 20.04"
+}
+
+variable "type" {
+ default = "t2.micro"
+ description = "Size of VM"
+}
